b1063fbafe211d833536e06b8c51cf3de5513e3c
EduardaAndrade03 <eduardaamaria2608@gmail.com>
2025-11-03 21:23:26 -0300

    terminando atv

--- funcoes.py
+++ funcoes.py
@@ -1,44 +1,39 @@
-# Parte 1 — Estrutura inicial
-# 1. Crie uma pasta chamada “bibliotecaPython”.
-# 2. Dentro dela, crie o arquivo funcoes.py.
-# o Crie as seguintes funções:
-# ▪ adicionar_livro(listaLivros): solicita título, autor e status (por padrão
-# “disponível”).
-# ▪ emprestar_livro(listaLivros): altera o status de um livro para
-# “emprestado”.
-# ▪ devolver_livro(listaLivros): altera o status de um livro para “disponível”.
-# ▪ exibir_livros(listaLivros): mostra todos os livros e seus status.
+from tabulate import tabulate
+
+tabelalivros = [] 
 
 livros=[]
 
 
 def adicionar_livro(titulo):
     livro={}
-    autor=str(input(f"digite o autor do {titulo} "))
+    autor=str(input(f"digite o autor do livro {titulo}: "))
     status="disponivel"
     livro={
         "titulo":titulo,
         "autor":autor,
         "status":status
     }
-    livros.append(livro)
+    tabelalivros.append(livro)
     return "livro cadastrado!"
 
 def emprestar_livro():
     livro=str(input("qual livro deseja adiquirir: "))
 
-    for l in livros:
+    for l in tabelalivros:
         if l["titulo"]==livro:
             l["status"]="emprestado"
             return "livro emprestado! (devolva viu)"
+        else:
+            return "Houve algum problema.."
             
 def devolver_livro():
     livro=str(input("qual livro deseja devolver: "))
 
-    for l in livros:
+    for l in tabelalivros:
         if l["titulo"]==livro:
             l["status"]="disponível"
             return "livro devolvido! (muito bem)"
 
 def exibir_livros():
-    return livros
+    return tabulate(tabelalivros, headers = "keys", tablefmt="grid")
